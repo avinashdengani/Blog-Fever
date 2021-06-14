ba7a8e1b4f128cc40443d9c91c68c21360b0a722
Avinash Dengani <avinashdengani123@gmail.com>
2021-06-14 20:02:00 +0530

    Made changes for code reuabilty in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,5 @@
 @extends('layouts.admin-panel.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <p>Sidebar</p>
-            </div>
-
-            <div class="col-md-8">
-                <p>Logged in</p>
-            </div>
-        </div>
-    </div>
+    <p>Logged in</p>
 @endsection
