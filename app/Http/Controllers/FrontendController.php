a90bce30d69bd1a10f6f425f34787db5ddd004e7
Avinash Dengani <avinashdengani123@gmail.com>
2021-06-30 00:13:47 +0530

    Frontend Controller to controll incoming data from db

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::simplePaginate(9);
+        $tags = Tag::all();
+        $categories = Category::all();
 
-        return view('blogs.index', compact(['posts']));
+        return view('blogs.index', compact(['posts', 'tags', 'categories']));
     }
 }
