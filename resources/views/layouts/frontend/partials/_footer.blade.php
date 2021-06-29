6954ce3bdaae2fa40d340eb9d578bed62fe40001
Avinash Dengani <avinashdengani123@gmail.com>
2021-06-30 00:12:21 +0530

    Footer for blog index

---
+++ resources/views/layouts/frontend/partials/_footer.blade.php
@@ -0,0 +1,46 @@
+<!-- Footer Area
+=====================================-->
+<footer id="footer" class="footer-one center-block bg-light pt50 pb30 ">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-2 col-xs-12 mb25">
+                <a href="{{asset('/')}}" class="navbar-brand-footer center-block" style="width: 200px; background:none;">Blog-Fever</a>
+                <div class="copyright center-block">&copy; 2021. All rights reserved.</div>
+            </div>
+
+            <div class="col-md-8 col-xs-12 text-center">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <ul class=" bb-solid-1">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Post</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-12 mt25">
+                        <ul>
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">Knowledgebase</a></li>
+                            <li><a href="#">Term of Service</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">FAQs</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-xs-12 " style="display: flex; justify-content:center;">
+                <div class="social-container ">
+                    <ul class="footer-social text-center" style="width: 200px;">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-github"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
