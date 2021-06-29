99803e413197dd34930186cce5c7159d74b1652b
Avinash Dengani <avinashdengani123@gmail.com>
2021-06-30 00:14:25 +0530

    Made changes in simple-default pagination style

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -1,19 +1,19 @@
 @if ($paginator->hasPages())
-    <nav>
+    <div class="row m25 animated " data-animation="fadeInUp" data-animation-delay="100" style="display: flex; flex-direction:column;">
         <ul class="pagination">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+            <li class="disabled" aria-disabled="true"><a class="button button-sm button-pasific pull-left hover-skew-backward">Old Entries</a></li>
             @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="button button-sm button-pasific pull-left hover-skew-backward"> Old Entries</a></li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next" class="button button-sm button-pasific pull-right hover-skew-forward">New Entries</a></li>
             @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+            <li class="disabled" aria-disabled="true"><a class="button button-sm button-pasific pull-right hover-skew-forward ">New Entries</a></li>
             @endif
         </ul>
-    </nav>
+    </div>
 @endif
