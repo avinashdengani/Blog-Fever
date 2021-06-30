1f2038ffca9752cc97933c72d2c43de1f26d06dd
Avinash Dengani <avinashdengani123@gmail.com>
2021-06-30 22:01:57 +0530

    Added Links to redirect on post.blade.php page

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -23,15 +23,15 @@
         @foreach ($posts as $post )
             <div class="col-md-4 col-sm-6 col-xs-12 mb50">
                 <h4 class="blog-title">
-                    <a href="#">{{ $post->title }}</a>
+                    <a href="{{ route('blogs.show', $post->id) }}">{{ $post->title }}</a>
                 </h4>
                 <div class="blog-three-attrib">
                     <span class="icon-calendar"></span>Dec 15 2019 |
                     <span class=" icon-pencil"></span><a href="#">{{$post->author->name}}</a>
                 </div>
-                <img src="{{asset($post->image)}}" class="img-responsive" alt="image blog">
+                <img src="{{asset($post->image_path)}}" class="img-responsive" alt="post image"  >
                 <p class="mt25">{{$post->excerpt}}</p>
-                <a href="#" class="button button-gray button-xs">Read More <i class="fa fa-long-arrow-right"></i></a>
+                <a href="{{ route('blogs.show', $post->id) }}" class="button button-gray button-xs">Read More <i class="fa fa-long-arrow-right"></i></a>
             </div>
         @endforeach
     </div>
