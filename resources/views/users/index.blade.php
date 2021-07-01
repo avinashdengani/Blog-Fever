50048f332b4683f609b3e8f3358c0d38774947f4
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-02 00:52:14 +0530

    Added image column and displayed Gravatar image in User Index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,24 +3,28 @@
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h2>users</h2>
+        <h2>Users</h2>
     </div>
     <div class="card-body">
         <table class="table">
             <thead>
                 <tr>
+                    <th scope="col">Image</th>
                     <th scope="col">Name</th>
-                    <th scope="col">email</th>
-                    <th scope="col">posts</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Posts</th>
+                    <th scope="col">Role</th>
                     <th scope="col">Actions</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($users as $user)
                 <tr>
+                    <td><img src="{{ $user->gravatar_image }}" alt="User-image" width="80px"></td>
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->posts->count() }}</td>
+                    <td>{{ $user->role }}</td>
                     <td>
                         @if (! $user->isAdmin())
                             <form action="{{ route('users.make-admin', $user->id) }}" method="POST">
