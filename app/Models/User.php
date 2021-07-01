7a535c52bdb27f3b55d927bc330cc68fb66b9a2e
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-01 20:58:24 +0530

    Added role in fillable, and aaded a method to get no. of count for posts and made a method isAdmin to check whether user is admin or not

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    const ADMIN = 'admin';
+    const AUTHOR = 'author';
     /**
      * The attributes that are mass assignable.
      *
@@ -20,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -40,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === self::ADMIN;
+    }
 }
