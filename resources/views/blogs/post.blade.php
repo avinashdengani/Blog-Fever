b5285fce967271ae0c3a295bfdc893558dc02b60
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-03 01:20:50 +0530

    Added Comment Section using Disqus

--- resources/views/blogs/post.blade.php
+++ resources/views/blogs/post.blade.php
@@ -24,8 +24,8 @@
         <h2 class="color-dark"><a href="#">{{ $post->title }}</a></h2>
         <div class="blog-three-attrib">
             <div><i class="fa fa-calendar"></i>{{ $post->published_at->diffForHumans() }} </div> |
-            <div><i class="fa fa-pencil"></i><a href="#">{{ $post->author->name }}</a></div> |
-            <div><i class="fa fa-comment-o"></i><a href="#">90 Comments</a></div> |
+            <div><i class="fa fa-pencil"></i><a href="#user-image">{{ $post->author->name }}</a></div> |
+            <div><i class="fa fa-comment-o"></i><a href="#comment-box">Comments</a></div> |
             <div><a href="#"><i class="fa fa-thumbs-o-up"></i></a>150 Likes</div> |
             <div>
                 Share:  <a href="#"><i class="fa fa-facebook-official"></i></a>
@@ -53,7 +53,7 @@
     </div>
 
 
-    <div class="blog-post-author mb50 pt30 bt-solid-1">
+    <div class="blog-post-author mb50 pt30 bt-solid-1" id="user-image">
         <img src="{{ $post->author->gravatar_image }}" alt="user-image">
         <span class="blog-post-author-name">{{ $post->author->name }}</span> <a href="https://twitter.com/booisme"><i class="fa fa-twitter"></i></a>
         <p>
@@ -61,4 +61,27 @@
         </p>
     </div>
 
+<div class="blog-post-comment-container" id="comment-box">
+    <div id="disqus_thread"></div>
+    <script>
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+
+        var disqus_config = function () {
+            this.page.url = "{{ route('blogs.show', $post->id) }}";  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = {{ $post->id }}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://blog-fever.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+
+</div>
 @endsection
