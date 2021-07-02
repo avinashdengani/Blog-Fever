f34fd23bd39eb3b6f1035a07212ffbadbcfc9384
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-02 20:00:49 +0530

    Initialized VerifycategoriesCount middleware in kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use App\Http\Middleware\ValidateAuthor;
 use App\Http\Middleware\VerifyAdmin;
+use App\Http\Middleware\VerifyCategoriesCount;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -66,5 +67,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'verifyAdmin' => VerifyAdmin::class,
         'validateAuthor' => ValidateAuthor::class,
+        'verifyCategoriesCount' => VerifyCategoriesCount::class,
     ];
 }
