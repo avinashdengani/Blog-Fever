bf07334d9a9b91c647e15d7274fde505897fe734
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-02 19:28:19 +0530

    Initialized ValidateAuthor in Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ValidateAuthor;
 use App\Http\Middleware\VerifyAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'verifyAdmin' => VerifyAdmin::class,
+        'validateAuthor' => ValidateAuthor::class,
     ];
 }
