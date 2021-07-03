6bb8f452123e48c55563f205a0b63120675432d9
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-03 16:39:19 +0530

    Deleted ValidateAuthor middleware and added functionality to middlewares for Edit and Delete

--- app/Http/Middleware/validateUserForDelete.php
+++ app/Http/Middleware/validateUserForDelete.php
@@ -16,6 +16,11 @@ class validateUserForDelete
      */
     public function handle(Request $request, Closure $next)
     {
+        if(is_object($request->post)) {
+            if(! ( ($request->post->user_id == auth()->id()) ||  auth()->user()->isAdmin())) {
+                return redirect(abort(401));
+            }
+        }
         return $next($request);
     }
 }
--- app/Http/Middleware/validateUserForEdit.php
+++ app/Http/Middleware/validateUserForEdit.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Post;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,15 @@ class validateUserForEdit
      */
     public function handle(Request $request, Closure $next)
     {
+        if(is_object($request->post)) {
+            if(! ($request->post->user_id == auth()->id())) {
+                return redirect(abort(401));
+            }
+        } else if(is_numeric($request->post)) {
+            if(! (Post::withTrashed()->findOrFail($request->post())->user_id == auth()->id)) {
+                return redirect(abort(401));
+            }
+        }
         return $next($request);
     }
 }
